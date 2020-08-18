c9cf63bc48475e6a7fb361cc9b0cc7991e8a725a
trisha-n <trisha2006mummy@gmail.com>
2020-08-18 15:56:02 +0530

    added animations

--- js/Game.js
+++ js/Game.js
@@ -34,6 +34,10 @@ class Game {
     car3 = createSprite(500,200);
     car4 = createSprite(700,200);
     cars = [car1, car2, car3, car4];
+    car1.addImage("car1", car1img);
+    car2.addImage("car2", car2img);
+    car3.addImage("car3", car3img);
+    car4.addImage("car4", car4img);
   }
 
   play(){
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background("#c68767");
+      image(trackimg, 0, -displayHeight * 4 , displayWidth, displayHeight*5)
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -56,7 +62,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 250;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -74,11 +80,19 @@ class Game {
 
     }
 
-    if(keyIsDown(UP_ARROW) && player.index !== null){
+    if(keyIsDown(UP_ARROW) && player.index !== null && player.distance < 4300){
       player.distance +=10
       player.update();
     }
+    if(player.distance >= 4230){
+      gameState = 2;
+    }
 
     drawSprites();
+  }
+  end(){
+    console.log("game end");
+    game.update(2);
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -8,8 +8,15 @@ var database;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
-
+var cars, car1, car1img, car2, car2img, car3, car3img, car4, car4img, trackimg, groundimg;
+function preload(){
+  car1img = loadImage("images/car1.png")
+  car2img = loadImage("images/car2.png")
+  car3img = loadImage("images/car3.png")
+  car4img = loadImage("images/car4.png")
+  trackimg = loadImage("images/track.jpg")
+  groundimg = loadImage("images/ground.png")
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +34,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
